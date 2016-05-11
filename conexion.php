c001fdefe4e03ff82a291dda1b3c42df09305820
Juan Diego H <jdiegoh3@users.noreply.github.com>
2016-05-11 09:56:37 -0500

    Funcionando el loggeo con logout y todo

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
-<?php
-// datos para la coneccion a mysql
-define('DB_SERVER','localhost');
-define('DB_NAME','pizzeria');
-define('DB_USER','root');
-define('DB_PASS','');
+<?php 
+// datos para la coneccion a mysql 
+define('DB_SERVER','localhost'); 
+define('DB_NAME','pizz'); 
+define('DB_USER','root'); 
+define('DB_PASS',''); 
 
-$con = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
-mysql_select_db(DB_NAME,$con);
+$con = mysql_connect(DB_SERVER,DB_USER,DB_PASS); 
+mysql_select_db(DB_NAME,$con); 
 ?>
